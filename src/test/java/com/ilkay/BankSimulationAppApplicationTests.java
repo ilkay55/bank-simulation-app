9837a809562b94f101e4ab003b762606ec148ee0
ilkay55 <akinsamsun@gmail.com>
2023-10-27 15:16:00 +0300

    Restarting the project due to intellij malfunction

---
+++ src/test/java/com/ilkay/BankSimulationAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ilkay;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BankSimulationAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
