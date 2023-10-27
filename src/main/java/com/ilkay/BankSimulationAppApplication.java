9a0ddb095f8a0c13418a2ee2c880b24c7e8ec083
ilkay55 <akinsamsun@gmail.com>
2023-10-27 15:20:50 +0300

    Restarting the project due to intellij malfunction

--- src/main/java/com/ilkay/BankSimulationAppApplication.java
+++ src/main/java/com/ilkay/BankSimulationAppApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class BankSimulationAppApplication {
-
+//restatrting
     public static void main(String[] args) {
         SpringApplication.run(BankSimulationAppApplication.class, args);
     }
