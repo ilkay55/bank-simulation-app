713760215284a6e38a428a241ea37da9699cfeca
ilkay55 <akinsamsun@gmail.com>
2023-11-10 16:58:43 +0300

    post method added

--- src/main/java/com/ilkay/controller/AccountController.java
+++ src/main/java/com/ilkay/controller/AccountController.java
@@ -1,14 +1,17 @@
 package com.ilkay.controller;
 
+import com.ilkay.enums.AccountType;
+import com.ilkay.model.Account;
 import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.*;
 import com.ilkay.service.AccountService;
 
-@Controller //@Controller already has @Component functionality, that`s why we do not need to put an extra @Component annotation here
+import java.util.Date;
+
+@Controller
+//@Controller already has @Component functionality, that`s why we do not need to put an extra @Component annotation here
 public class AccountController {
 
       /*
@@ -24,17 +27,38 @@ public class AccountController {
 //we need a regular java method which has always String as Return type
 // because we are returning html path  (location of my html or thymeleaf path)
 
-//    @RequestMapping(value="/index", method = RequestMethod.GET)
+    //    @RequestMapping(value="/index", method = RequestMethod.GET)
 //    or if we do not need info from user we simply use GetMapping
     @GetMapping("/index")
-    public String getIndexPage(Model model){
+    public String getIndexPage(Model model) {
 
-        model.addAttribute("accountList",accountService.listAllAccount());
+        model.addAttribute("accountList", accountService.listAllAccount());
 
-return "account/index";
+        return "account/index";
 
 //to show the location of the page, we assume templates directory as our root.
 // So that we do not need to indicate it is under resources/templates
 
+    }
+
+    @GetMapping("/create-form")
+    public String getCreateForm(Model model){
+        //we need to provide empty account object
+        model.addAttribute("account", Account.builder().build());
+        //we need to provide accountType enum info for filling the dropdown options
+        model.addAttribute("accountTypes", AccountType.values());
+
+        return "account/create-account";
+    }
+    //create a method to capture information from ui
+    //print them on the console.
+    //trigger createNewAccount method, create the account based on the user input.
+    //once user created return back to the index page.
+
+    @PostMapping("/create")
+    public String createAccount(@ModelAttribute("account") Account account){
+        System.out.println(account);
+        accountService.createNewAccount(account.getBalance(),new Date(),account.getAccountType(),account.getUserId());
+        return "redirect:/index";
     }
 }
